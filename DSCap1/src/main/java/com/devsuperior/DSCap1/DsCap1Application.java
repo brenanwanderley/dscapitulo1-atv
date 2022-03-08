b4ff8d5dc34bbea1a4ef0c9a1137c2545a6e8f99
Brenan <brenanwanderley@gmail.com>
2022-03-08 15:57:03 -0300

    Repo Atividade 1 - criado

---
+++ DSCap1/src/main/java/com/devsuperior/DSCap1/DsCap1Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.DSCap1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsCap1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsCap1Application.class, args);
+	}
+
+}
